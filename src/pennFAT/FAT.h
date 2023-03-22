00fe3532ab345db5c1aaee22c85da44ffe498b51
Louis-sf <shufanwl@gmail.com>
2023-03-22 14:52:05 -0400

    reorg directory

---
+++ src/pennFAT/FAT.h
@@ -0,0 +1,44 @@
+#ifndef FAT_H
+#define FAT_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+typedef struct FAT {
+    char* f_name;
+    uint8_t block_num;
+    uint32_t block_size;
+    uint32_t entry_num;
+    uint32_t 
+
+}FAT;
+
+typedef struct directory {
+    char name[32];
+    uint32_t size;
+    uint16_t firstBlock;
+    uint8_t type;
+    uint8_t perm;
+    time_t mtime; 
+} directoryEntry;
+
+typedef struct dir_node {
+    directoryEntry *entry;
+    struct dir_node* next;
+} dir_node;
+
+
+
+dir_node * NewDirNode(
+    char* f_name,
+    uint32_t size,
+    uint16_t firstBlock,
+    uint8_t type,
+    uint8_t perm,
+    time_t time
+);
+
+void free_dir_node(dir_node *node);
+
+
---
+++ src/pennFAT/macro.h
@@ -0,0 +1,15 @@
+#define PENNFAT_PROMPT "pennfat> "
+#define PROMPT_LENGTH 9
+
+#define UNKNOWN_FILETYPE 0
+#define REGULAR_FILETYPE 1
+#define DIRECTORY_FILETYPE 2
+#define SYMBOLIC_LINK 4
+
+#define NO_PERMS 0
+#define WRITE_PERMS 2
+#define READ_PERMS 4
+#define READ_EXCUTABLE_PERMS 5
+#define READ_WRITE_PERMS 6
+#define READ_WRITE_EXCUTABLE 7
+
