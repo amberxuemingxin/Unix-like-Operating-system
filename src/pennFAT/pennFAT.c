f3540b34f6203952f537b9ec53c03b4f316327d7
Louis-sf <shufanwl@gmail.com>
2023-03-22 14:52:46 -0400

    reorg directory

---
+++ src/pennFAT/pennFAT.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <time.h>
+// #include "directory.h"
+// int createFAT() {
+
+// }
+
+// int cleanFAT() {
+
+// }
+
+// int deleteFAT() {
+
+// }
+
+// int append_entry() {
+
+// }
+
+
+int main(int argc, char *argv[]) {
+    while (1) {
+
+    }
+}
