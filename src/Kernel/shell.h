651e3b067d9a86b8846b88c644c672a60bcfda1e
DonglunHe <hedonglun123@gmail.com>
2023-04-17 20:05:02 -0400

    comments and debug

--- src/Kernel/shell.h
+++ src/Kernel/shell.h
@@ -1,3 +1,10 @@
+/**
+ * @file shell.h
+ * @brief The main shell loop that keeps blocking until receives user input
+ *
+ */
 #include "kernel.h"
-
+/**
+ * @brief The main loop
+ */
 void shell_loop();
